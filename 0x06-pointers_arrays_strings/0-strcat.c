ed372ca6e6cbfddd48b4ddd291cc7f33cc410c86
MichaelRobsonZA <michaelrobsonza@gmail.com>
2023-03-03 04:53:47 -0800

    commit

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -13,18 +13,7 @@ char *_strcat(char *dest, char *src)
 
 	i = 0;
 	while (dest[i] != '\0')
-	{
-		i++;
-	}
-	j = 0;
-	while (src[j] != '\0')
-	{
-		dest[i] = src[j];
-		i++;
-		j++;
-	}
-
-	dest[i] = '\0';
-	return (dest);
+	
+		return (dest);
 }
 
