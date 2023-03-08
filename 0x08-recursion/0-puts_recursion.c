392a51d07bfe7d800d272c7c03f1ccacf50f4d40
MichaelRobsonZA <michaelrobsonza@gmail.com>
2023-03-08 02:00:41 -0800

    fix betty issues

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,15 +8,15 @@
 void _puts_recursion(char *s)
 {
     /* If the current character is not '\0', print it and recurse. */
-    if (*s != '\0')
-    {
-        _putchar(*s);
-        _puts_recursion(s + 1);
-    }
-    /* If the current character is '\0', print a newline character. */
-    else
-    {
-        _putchar('\n');
-    }
+if (*s != '\0')
+	{
+	_putchar(*s);
+	_puts_recursion(s + 1);
+	}
+/* If the current character is '\0', print a newline character. */
+	else
+	{
+	_putchar('\n');
+	}
 }
 
