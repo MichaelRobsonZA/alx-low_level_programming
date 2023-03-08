7118cf9aa86ce630a4c41a32e8472baec247127f
MichaelRobsonZA <michaelrobsonza@gmail.com>
2023-03-08 01:50:44 -0800

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - Prints a string recursively, followed by a new line.
+ *
+ * @s: The string to print.
+ */
+void _puts_recursion(char *s)
+{
+    /* If the current character is not '\0', print it and recurse. */
+    if (*s != '\0')
+    {
+        _putchar(*s);
+        _puts_recursion(s + 1);
+    }
+    /* If the current character is '\0', print a newline character. */
+    else
+    {
+        _putchar('\n');
+    }
+}
+
