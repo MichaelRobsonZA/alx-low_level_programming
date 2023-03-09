70e54012b7f13c1e7108e00a6bdc91e5c31d3046
MichaelRobsonZA <michaelrobsonza@gmail.com>
2023-03-09 01:16:09 -0800

    commit

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -7,12 +7,12 @@
  */
 int helper(int n, int i)
 {
-    if (i * i == n)
-        return i;
-    else if (i * i > n)
-        return -1;
-    else
-        return helper(n, i + 1);
+	if (i * i == n)
+		return (i);
+	else if (i * i > n)
+		return (-1);
+	else
+		return (helper(n, i + 1));
 }
 
 /**
@@ -23,8 +23,8 @@ int helper(int n, int i)
  */
 int _sqrt_recursion(int n)
 {
-    if (n < 0)
-        return -1;
-    else
-        return helper(n, 0);
+	if (n < 0)
+		return (-1);
+	else
+		return (helper(n, 0));
 }
