3722c684df785ca8bb541b6690be4d2e94bf533f
MichaelRobsonZA <michaelrobsonza@gmail.com>
2023-03-11 02:49:48 -0800

    commit

--- 0x09-static_libraries/CompileLinkLib.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Compile all C files into object files
-gcc -c *.c
-# Archive the object files into a static library
-ar rc libmy.a *.o
-# Index the library for faster linking
-ranlib libmy.a
