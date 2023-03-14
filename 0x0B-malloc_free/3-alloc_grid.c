1babf242350a897591ccd4f86356dbe7723f6a88
MichaelRobsonZA <michaelrobsonza@gmail.com>
2023-03-14 06:17:20 -0700

    commit

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -11,24 +11,23 @@
  */
 int **alloc_grid(int width, int height)
 {
-    if (width <= 0 || height <= 0)
-        return NULL;
+	if (width <= 0 || height <= 0)
+		return (NULL);
 
-    int **arr = calloc(height, sizeof(int *));
+	int **arr = calloc(height, sizeof(int *));
 
-    if (arr == NULL)
-        return NULL;
+	if (arr == NULL)
+		return (NULL);
 
-    for (int i = 0; i < height; i++)
-    {
-        arr[i] = calloc(width, sizeof(int));
-        if (arr[i] == NULL)
-        {
-            for (int j = 0; j < i; j++)
-                free(arr[j]);
-            free(arr);
-            return NULL;
-        }
-    }
-    return arr;
+	for (int i = 0; i < height; i++)
+	{
+		arr[i] = calloc(width, sizeof(int));
+		if (arr[i] == NULL)
+	{	for (int j = 0; j < i; j++)
+		free(arr[j]);
+		free(arr);
+		return (NULL);
+	}
+	}
+	return (arr);
 }
