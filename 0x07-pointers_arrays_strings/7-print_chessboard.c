970f545e33ff7ef5bb8fab852953881d0b86fdc0
MichaelRobsonZA <michaelrobsonza@gmail.com>
2023-03-17 05:14:47 -0700

    correction

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -8,15 +8,14 @@
  */
 void print_chessboard(char (*a)[8])
 {
-    int row, col;
-
-    for (row = 0; row < 8; row++)
-    {
-        for (col = 0; col < 8; col++)
-        {
-            _putchar(a[row][col]);
-        }
-        _putchar('\n');
-    }
+	int row, col;
+	for (row = 0; row < 8; row++)
+	{
+		for (col = 0; col < 8; col++)
+		{
+			_putchar(a[row][col]);
+		}
+		_putchar('\n');
+	}
 }
 
