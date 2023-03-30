ab7bd67576c882b5d438039d14bee3e02ead0550
MichaelRobsonZA <michaelrobsonza@gmail.com>
2023-03-30 05:17:18 -0700

    Add function to free a singly linked list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+#include "lists.h"
+
+/**
+ * free_list - frees a list_t list
+ * @head: pointer to the head node
+ */
+void free_list(list_t *head)
+{
+    list_t *current;
+
+    while (head != NULL)
+    {
+        current = head;
+        head = head->next;
+        free(current->str);
+        free(current);
+    }
+}
+
