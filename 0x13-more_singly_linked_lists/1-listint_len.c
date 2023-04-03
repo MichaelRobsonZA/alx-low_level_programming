f54a4d8a7f28099f5d95a20a4fb69c507c07d2a0
MichaelRobsonZA <michaelrobsonza@gmail.com>
2023-04-03 07:16:53 -0700

    Add function to get the number of elements in a list

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * listint_len - returns the number of elements in a linked listint_t list.
+ * @h: pointer to the head of the list
+ *
+ * Return: the number of elements in the list
+ */
+size_t listint_len(const listint_t *h)
+{
+    size_t count = 0; /* initialize a counter variable to 0 */
+
+    /* iterate over the list with a loop, until the pointer is NULL */
+    while (h != NULL)
+    {
+        count++; /* increment the counter for each node */
+        h = h->next; /* move to the next node */
+    }
+
+    return (count); /* return the counter value as the list length */
+}
+
