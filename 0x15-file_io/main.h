7eb12c92861e68ff66d25f95dab3263086539e06
MichaelRobsonZA <michaelrobsonza@gmail.com>
2023-04-09 04:27:56 -0700

    commit

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -4,8 +4,14 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-ssize_t read_textfile(const char *filename, size_t letters);
+
 int _putchar(char c);
+
+
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+
+
 
 #endif /* MAIN_H */
 
