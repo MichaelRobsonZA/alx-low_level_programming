80e3c6fbb5e05a0457c5250173e5c964c0622e3b
MichaelRobsonZA <michaelrobsonza@gmail.com>
2023-04-09 04:26:32 -0700

    Added create_file function with file creation and writing functionality

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,40 +1,56 @@
 #include "main.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 /**
- * create_file - Creates a file and writes text content to it
- * @filename: Name of the file to create
- * @text_content: Null-terminated string to write to the file
+ * create_file - creates a file with given name and writes given content to it
+ * @filename: name of the file to create
+ * @text_content: content to write to the file
  *
  * Return: 1 on success, -1 on failure
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, written_bytes, text_length = 0;
+	int fd, len = 0, written;
 
-	/* Check if filename or text_content is NULL */
-	if (!filename)
+	/* Check if filename is NULL */
+	if (filename == NULL)
 		return (-1);
 
-	/* Open the file in write-only mode, create if it does not exist, truncate if it does */
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-	if (fd == -1) /* If open() fails, return -1 */
-		return (-1);
-
-	if (text_content) /* Check if text_content is not NULL */
+	/* Calculate length of text_content */
+	if (text_content != NULL)
 	{
-		/* Calculate the length of the text content */
-		while (text_content[text_length])
-			text_length++;
-
-		/* Write text_content to the file */
-		written_bytes = write(fd, text_content, text_length);
-		if (written_bytes == -1) /* If write() fails, return -1 */
-			return (-1);
+		while (text_content[len] != '\0')
+			len++;
 	}
 
-	/* Close the file descriptor */
+	/* Open file with write-only mode, create it if it doesn't exist, truncate it if it does */
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+		return (-1);
+
+	/* Write text_content to file if it's not NULL */
+	if (text_content != NULL)
+	{
+		/* Write text_content to file descriptor */
+		written = write(fd, text_content, len);
+
+		/* Check if write was successful */
+		if (written == -1 || written != len)
+		{
+			/* If write failed, close file descriptor and return -1 */
+			close(fd);
+			return (-1);
+		}
+	}
+
+	/* Close file descriptor */
 	close(fd);
 
+
+	/* Return 1 to indicate success */
 	return (1);
 }
 
