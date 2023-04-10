e03ba7fc593ee7927b26531526ec1d4d1f405ae1
MichaelRobsonZA <michaelrobsonza@gmail.com>
2023-04-09 20:49:58 -0700

    fixed code indent for conditional statement

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,42 +15,42 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd, len = 0, written;
 
-	/* Check if filename is NULL */
+/* Check if filename is NULL */
 	if (filename == NULL)
 		return (-1);
 
-	/* Calculate length of text_content */
+/* Calculate length of text_content */
 	if (text_content != NULL)
 	{
 		while (text_content[len] != '\0')
 			len++;
 	}
 
-	/* Open file with write-only mode, create it if it doesn't exist, truncate it if it does */
+/* Open file with write-only mode, create it if it doesn't exists */
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
 
-	/* Write text_content to file if it's not NULL */
+/* Write text_content to file if it's not NULL */
 	if (text_content != NULL)
 	{
-		/* Write text_content to file descriptor */
+/* Write text_content to file descriptor */
 		written = write(fd, text_content, len);
 
-		/* Check if write was successful */
+/* Check if write was successful */
 		if (written == -1 || written != len)
 		{
-			/* If write failed, close file descriptor and return -1 */
+/* If write failed, close file descriptor and return -1 */
 			close(fd);
 			return (-1);
 		}
 	}
 
-	/* Close file descriptor */
+/* Close file descriptor */
 	close(fd);
 
 
-	/* Return 1 to indicate success */
+/* Return 1 to indicate success */
 	return (1);
 }
 
