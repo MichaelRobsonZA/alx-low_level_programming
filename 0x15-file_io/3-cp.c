9893d2cb09610766100333fa13db64b52cd78569
MichaelRobsonZA <michaelrobsonza@gmail.com>
2023-04-13 01:03:22 -0700

    Corrrections

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,4 +1,10 @@
 #include "main.h"
+#include <stdio.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <unistd.h>
+
 
 /**
  * main - copies the content of a file to another file
