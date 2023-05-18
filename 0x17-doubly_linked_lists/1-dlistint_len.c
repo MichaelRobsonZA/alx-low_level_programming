3477afbae1bfe1698ba029d1e84ddc65b788a2bb
MichaelRobsonZA <michaelrobsonza@gmail.com>
2023-05-18 04:52:20 -0700

    betty corrections

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -8,14 +8,14 @@
  */
 size_t dlistint_len(const dlistint_t *h)
 {
-    size_t num_nodes = 0;
+	size_t num_nodes = 0;
 
-    while (h != NULL)
-    {
-        num_nodes++;
-        h = h->next;
-    }
+	while (h != NULL)
+	{
+		num_nodes++;
+		h = h->next;
+	}
 
-    return (num_nodes);
+	return (num_nodes);
 }
 
--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -9,16 +9,16 @@
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-    unsigned int count = 0;
+	unsigned int count = 0;
 
-    while (head != NULL)
-    {
-        if (count == index)
-            return (head);
-        count++;
-        head = head->next;
-    }
+	while (head != NULL)
+	{
+		if (count == index)
+			return (head);
+		count++;
+		head = head->next;
+	}
 
-    return (NULL);
+	return (NULL);
 }
 
