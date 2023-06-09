03630d8008ff2aa6d00b4e7deafb718dbfc072d8
MichaelRobsonZA <michaelrobsonza@gmail.com>
2023-06-09 06:28:28 -0700

    required files and solutions to task 0 & 1

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,34 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - Creates a hash table.
+ *
+ * @size: The size of the array.
+ *
+ * Return: A pointer to newly created hash table.
+ *         NULL if something went wrong.
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+    hash_table_t *ht;
+
+    if (size == 0)
+        return (NULL);
+
+    ht = malloc(sizeof(hash_table_t));
+    if (ht == NULL)
+        return (NULL);
+
+    ht->size = size;
+
+    ht->array = calloc(size, sizeof(hash_node_t *));
+    if (ht->array == NULL)
+    {
+        free(ht);
+        return (NULL);
+    }
+
+    return (ht);
+}
+
